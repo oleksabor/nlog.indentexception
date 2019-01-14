4de1f871f4bb1639b437e7b796718d531cbcfa29
Oleksii Borodai <oleksii.borodai@quipu.de>
2019-01-14 16:37:32 +0200

    test and description

---
+++ readme.md
@@ -0,0 +1,21 @@
+I've asked a question about [exception layout renderer at SO](https://stackoverflow.com/questions/46565639/nlog-exception-layout-to-format-exception-type-message-and-stack-trace)
+
+However seems that writing custom layout renderer is not so complex
+So here i will keep IndentExceptionLayoutRenderer.cs source code
+
+This renderer allows to show exception using readable format (from my point of view) in the log (file or console)
+
+src\NLog.config is a sample configuration that shows how layout can be parameterized for console and file logging
+
+Below you can find IndentException class properties that can help you to adjust exception logging
+
+property name | description 
+-----|------
+Indent | what character(s) are placed before exception in the log (default is tab)
+StackTraceIndent | indent between each stack trace line (default is two tab characters)
+BeforeType | is written before exception type name (default [)
+AfterType | is written after exception type name (default ])
+Separator | separator between exception type and message
+LogStack | to log stack trace or not (for console logger e.g.)
+
+
---
+++ src/NLogTest.cs
@@ -0,0 +1,67 @@
+using NLog;
+using System;
+using System.Collections.Generic;
+
+namespace YourNamespace.NLog.Extention.Test
+{
+	using YourNamespace.NLog.Extention.Test.Classes;
+
+	class Program
+	{
+		static ILogger Log = LogManager.GetCurrentClassLogger();
+
+		static void Main(string[] args)
+		{
+			Log.Debug("starting");
+			try
+			{
+				new UnitOfWork().tryException();
+			}
+			catch (Exception e)
+			{
+				Log.Error(e, "failed to start NLogTest");
+				Console.ReadKey();
+			}
+			Log.Debug("the end");
+		}
+	}
+}
+
+namespace YourNamespace.NLog.Extention.Test.Classes
+{
+	class UnitOfWork
+	{
+		static ILogger Log = LogManager.GetCurrentClassLogger();
+
+		public void innerException()
+		{
+			throw new KeyNotFoundException("innerException");
+		}
+
+		public void outerException()
+		{
+			try
+			{
+				innerException();
+			}
+			catch (Exception e)
+			{
+				throw new ArgumentException("outer exception", e);
+			}
+		}
+
+		public void tryException()
+		{
+			try
+			{
+				outerException();
+			}
+			catch (Exception e)
+			{
+				Log.Error(e, "tryException failure");
+				throw new ArgumentException("bad try", e);
+			}
+		}
+
+	}
+}
